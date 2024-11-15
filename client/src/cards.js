f4c7d9dd7d9cacd94ab7ab8f28c1d14a56186dd3
Fred Moore <frederic_archer@hotmail.com>
2024-11-15 17:45:15 +0000

    updated attribute balances

--- client/src/cards.js
+++ client/src/cards.js
@@ -1,122 +1,257 @@
+// export const cards = [
+//   {
+//     name: 'Q-Tip Man',
+//     attributes: {
+//       size: 60,
+//       strength: 50,
+//       intelligence: 61,
+//       speed: 40,
+//       fright: 20,
+//     },
+//     imageURL: './characters/QtipMan.jpeg'
+//   },
+//   {
+//     name: 'Jelly Grandma',
+//     attributes: {
+//       size: 41,
+//       strength: 37,
+//       intelligence: 71,
+//       speed: 10,
+//       fright: 10,
+//     },
+//     imageURL: './characters/JellyGrandma.jpeg'
+//   },
+//   {
+//     name: 'Athletic Lapwing',
+//     attributes: {
+//       size: 20,
+//       strength: 33,
+//       intelligence: 64,
+//       speed: 90,
+//       fright: 15,
+//     },
+//     imageURL: './characters/AthleticLapwing.jpeg'
+//   },
+//   {
+//     name: 'Broccoli Scot',
+//     attributes: {
+//       size: 61,
+//       strength: 82,
+//       intelligence: 62,
+//       speed: 49,
+//       fright: 85,
+//     },
+//     imageURL: './characters/BroccoliScot.jpeg'
+//   },
+//   {
+//     name: 'Giant Preacher',
+//     attributes: {
+//       size: 91,
+//       strength: 69,
+//       intelligence: 15,
+//       speed: 5,
+//       fright: 62,
+//     },
+//     imageURL: './characters/GiantPreacher.jpeg'
+//   },
+//   {
+//     name: 'Pub Vampire',
+//     attributes: {
+//       size: 74,
+//       strength: 67,
+//       intelligence: 83,
+//       speed: 13,
+//       fright: 89,
+//     },
+//     imageURL: './characters/PubVamp.jpeg'
+//   },
+//   {
+//     name: 'Rat Lady',
+//     attributes: {
+//       size: 51,
+//       strength: 38,
+//       intelligence: 76,
+//       speed: 45,
+//       fright: 78,
+//     },
+//     imageURL: './characters/RatLady.jpeg'
+//   },
+//   {
+//     name: 'Eyelash',
+//     attributes: {
+//       size: 5,
+//       strength: 58,
+//       intelligence: 32,
+//       speed: 51,
+//       fright: 82,
+//     },
+//     imageURL: './characters/Eyelash.jpeg'
+//   },
+//   {
+//     name: 'Netflix Fiend',
+//     attributes: {
+//       size: 55,
+//       strength: 21,
+//       intelligence: 42,
+//       speed: 12,
+//       fright: 54,
+//     },
+//     imageURL: './characters/NetflixFiend.jpeg'
+//   },
+//   {
+//     name: 'Big Benny',
+//     attributes: {
+//       size: 100,
+//       strength: 93,
+//       intelligence: 7,
+//       speed: 48,
+//       fright: 39,
+//     },
+//     imageURL: './characters/BigBenny.jpeg'
+//   },
+//   {
+//     name: 'Chimney Penguin',
+//     attributes: {
+//       size: 34,
+//       strength: 66,
+//       intelligence: 60,
+//       speed: 62,
+//       fright: 56,
+//     },
+//     imageURL: './characters/ChimneyPenguin.jpeg'
+//   },
+//   {
+//     name: 'Accountant Eel',
+//     attributes: {
+//       size: 48,
+//       strength: 28,
+//       intelligence: 89,
+//       speed: 71,
+//       fright: 49,
+//     },
+//     imageURL: './characters/AccountantEel.jpeg'
+//   },
+// ];
+
 export const cards = [
   {
     name: 'Q-Tip Man',
     attributes: {
-      size: 60,
-      strength: 50,
-      intelligence: 61,
-      speed: 40,
-      fright: 20,
+      size: 58,
+      strength: 46,
+      intelligence: 63,
+      speed: 42,
+      fright: 30,
     },
     imageURL: './characters/QtipMan.jpeg'
   },
   {
     name: 'Jelly Grandma',
     attributes: {
-      size: 41,
-      strength: 37,
-      intelligence: 71,
-      speed: 10,
-      fright: 10,
+      size: 50,
+      strength: 43,
+      intelligence: 65,
+      speed: 40,
+      fright: 28,
     },
     imageURL: './characters/JellyGrandma.jpeg'
   },
   {
     name: 'Athletic Lapwing',
     attributes: {
-      size: 20,
-      strength: 33,
-      intelligence: 64,
-      speed: 90,
-      fright: 15,
+      size: 42,
+      strength: 41,
+      intelligence: 60,
+      speed: 55,
+      fright: 33,
     },
     imageURL: './characters/AthleticLapwing.jpeg'
   },
   {
     name: 'Broccoli Scot',
     attributes: {
-      size: 61,
-      strength: 82,
+      size: 60,
+      strength: 50,
       intelligence: 62,
-      speed: 49,
-      fright: 85,
+      speed: 44,
+      fright: 39,
     },
     imageURL: './characters/BroccoliScot.jpeg'
   },
   {
     name: 'Giant Preacher',
     attributes: {
-      size: 91,
-      strength: 69,
-      intelligence: 15,
-      speed: 5,
-      fright: 62,
+      size: 65,
+      strength: 47,
+      intelligence: 55,
+      speed: 37,
+      fright: 42,
     },
     imageURL: './characters/GiantPreacher.jpeg'
   },
   {
     name: 'Pub Vampire',
     attributes: {
-      size: 74,
-      strength: 67,
-      intelligence: 83,
-      speed: 13,
-      fright: 89,
+      size: 62,
+      strength: 48,
+      intelligence: 64,
+      speed: 35,
+      fright: 44,
     },
     imageURL: './characters/PubVamp.jpeg'
   },
   {
     name: 'Rat Lady',
     attributes: {
-      size: 51,
-      strength: 38,
-      intelligence: 76,
-      speed: 45,
-      fright: 78,
+      size: 54,
+      strength: 45,
+      intelligence: 66,
+      speed: 47,
+      fright: 40,
     },
     imageURL: './characters/RatLady.jpeg'
   },
   {
     name: 'Eyelash',
     attributes: {
-      size: 5,
-      strength: 58,
-      intelligence: 32,
-      speed: 51,
-      fright: 82,
+      size: 40,
+      strength: 49,
+      intelligence: 58,
+      speed: 48,
+      fright: 36,
     },
     imageURL: './characters/Eyelash.jpeg'
   },
   {
     name: 'Netflix Fiend',
     attributes: {
-      size: 55,
-      strength: 21,
-      intelligence: 42,
-      speed: 12,
-      fright: 54,
+      size: 46,
+      strength: 39,
+      intelligence: 57,
+      speed: 36,
+      fright: 34,
     },
     imageURL: './characters/NetflixFiend.jpeg'
   },
   {
     name: 'Big Benny',
     attributes: {
-      size: 100,
-      strength: 93,
-      intelligence: 7,
-      speed: 48,
-      fright: 39,
+      size: 64,
+      strength: 51,
+      intelligence: 56,
+      speed: 38,
+      fright: 32,
     },
     imageURL: './characters/BigBenny.jpeg'
   },
   {
     name: 'Chimney Penguin',
     attributes: {
-      size: 34,
-      strength: 66,
-      intelligence: 60,
-      speed: 62,
-      fright: 56,
+      size: 44,
+      strength: 44,
+      intelligence: 61,
+      speed: 49,
+      fright: 35,
     },
     imageURL: './characters/ChimneyPenguin.jpeg'
   },
@@ -124,10 +259,10 @@ export const cards = [
     name: 'Accountant Eel',
     attributes: {
       size: 48,
-      strength: 28,
-      intelligence: 89,
-      speed: 71,
-      fright: 49,
+      strength: 40,
+      intelligence: 68,
+      speed: 46,
+      fright: 37,
     },
     imageURL: './characters/AccountantEel.jpeg'
   },
